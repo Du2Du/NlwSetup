865f9781310a7157ae0015376264f1bd768eb935
Du2Du <eduardohilario.eha@gmail.com>
2023-01-16 22:13:40 -0300

    Configurando cors

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,10 +1,12 @@
 import fastify from "fastify";
-
+import cors from "@fastify/cors";
 const app = fastify();
+
+app.register(cors);
 
 app
   .listen({
     port: 8082,
   })
-  .then(() => console.log("💻 Server Iniciado 💻"))
+  .then(() => console.log("💻 Server Rodando 💻"))
   .catch((error) => console.log("🔥 Ocorreu algum erro 🔥\n", error));
