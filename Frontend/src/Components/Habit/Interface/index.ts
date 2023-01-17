fb76c3d59e803f400ca5aff17b2c770a8f21bb31
Du2Du <eduardohilario.eha@gmail.com>
2023-01-16 22:29:54 -0300

    Finalizando inicialização do frontend

---
+++ Frontend/src/Components/Habit/Interface/index.ts
@@ -0,0 +1,3 @@
+export interface HabitArgs {
+  completed: number;
+}
---
+++ Frontend/src/Components/Habit/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { HabitArgs } from "./Interface";
+
+export const Habit: React.FC<HabitArgs> = () => {
+  return <div>Text</div>;
+};
